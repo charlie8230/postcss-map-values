2931ba49f0bed9c944be93b394e27a8c09477881
Carlos <carlos@moranjr.com>
2019-06-18 01:37:59 -0400

    being testing config

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,15 @@
-import gulp from 'gulp';
-import postcss from 'gulp-postcss'
-import mapavalues from 'postcss-map-values'
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
 
-gulp.task('css',()=>{
-    
+
+var mapvalues = require('./index');
+var plugins =[mapvalues];
+gulp.task('default',()=>{
+    return gulp.src('./*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('./dist/'));
 });
