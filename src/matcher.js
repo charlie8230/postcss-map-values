33bbe92d9ebb76e5ec7659d9876492c1883f44ee
Carlos <carlos@moranjr.com>
2019-07-02 17:28:50 -0400

    remove logging

--- src/matcher.js
+++ src/matcher.js
@@ -2,7 +2,8 @@ module.exports = function matcher(val, valMap, excludeSubString){
     if (!(val && valMap)) return;
     for (let key in valMap) {
         if (valMap[key]===val) return key;
-        console.log('replacing', val, valMap[key], val.includes(valMap[key]));
+        //console.log('replacing', val, valMap[key], val.includes(valMap[key]));
+        // Split values on space before replacing
         if (typeof val === 'string' && val.includes && val.includes(valMap[key])) {
             
             let updated = val.replace(valMap[key], key);
