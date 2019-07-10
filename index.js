783d521eb42a757d3a2fc82361c35c2ec8063231
Carlos <carlos@moranjr.com>
2019-07-10 00:19:16 -0400

    Remove substring matches

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ var matcher = require('./src/matcher');
 module.exports = postcss.plugin('postcss-map-values', function (opts) {
   opts = opts || {}
   let valMap = opts.valueMap;
-  let excludeSubstring = opts.excludeSubstring;
   let total = 0;
   return function (css) {
     css.walkDecls(decl=>{
-        let upt = matcher(decl.value, valMap, excludeSubstring);
+        let upt = matcher(decl.value, valMap);
         if (upt) {
           decl.value = upt;
           total++;
